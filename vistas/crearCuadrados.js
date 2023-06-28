3b8bea87942291f06ef1e374e20c49b4ed5546b6
amanr <amanrique068@gmail.com>
2023-06-27 20:55:43 -0500

    cambios

--- vistas/crearCuadrados.js
+++ vistas/crearCuadrados.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
-    $("#cont").change( function () {
-        let contador = $("#cont").val();
-
-        let filas = Math.floor((contador * 2).sqrt);
-        let columnas;
-        for (let i = filas; i < contador * 2; i++) {
-            if ((contador * 2) % filas === 0) {
-                if ((contador * 2 / filas) % 2 === 1) {
-                    columnas = contador * 2 / filas;
-                    if (Math.abs(filas - columnas) === 1) {
+    $("#inputFoto").change( function () {
+        let contador = parseInt($("#cont").text());
+        console.log(contador);
+        let filas = Math.floor(Math.sqrt(contador * 2));
+        let columnas = 0;
+        for (let i = filas; i <= contador * 2; i++) {
+            console.log("nuevo numero:",filas);
+            if ((contador * 2) % i === 0) {
+                if ((contador * 2 / i) % 2 === 1) {
+                    columnas = contador * 2 / i;
+                    if (Math.abs(i - columnas) === 1) {
+                        filas = i;
                         break;
                     }
                 }
             }
-            if (i === contador * 2) {
-                filas = 0;
-                columnas = 0;
-                break;
-            }
         }
+        if (filas * columnas !== contador * 2){
+            filas = 0;
+            columnas = 0;
+        }
+        console.log(filas, columnas);
+        $("#tablero").val("");
+        $("#tablero").append("<div class='row row-cols-" + filas + " row-cols-lg-"+columnas+">");
+
         for (let k = 0; k < filas;k++){
-            $("#tablero").append("<div class=\"rectangulo\"></div>");
+            console.log("Fila", k);
+            $("#tablero").append("<div class='col'><div class='rectangulo p-3'>Row column</div></div>");
         }
         for (let k = 0; k < columnas;k++){
-            $("#tablero").append("<div class=\"rectangulo\"></div>");
+            console.log("Columna", k);
+            $("#tablero").append("<div class='col'><div class='rectangulo p-3'>Row column</div></div>");
         }
+        $("#tablero").append("</div>");
     })
 })
