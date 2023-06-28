08966e9692b67f2d5f867784286ab7274b4548db
amanr <amanrique068@gmail.com>
2023-06-28 04:09:28 -0500

    funcion de borrado listo

--- vistas/index.js
+++ vistas/index.js
@@ -1,4 +1,5 @@
 let cont = $("#cont");
+let contadorGeneral = -1;
 const contenedor = $("#contenedor");
 let cadena = [];
 
@@ -15,13 +16,14 @@ $("#botonAgregar").click(function () {
 })
 
 $("body").on("change", "#inputFoto", function (event){
+    contadorGeneral++;
     let archivo = event.target.files[0].name;
-    cadena.push(archivo);
-    console.log(cadena[0]);
+    cadena.push(archivo.split('.').shift());
     let extension = archivo.split('.').pop();
     if (extension === "png" && cont.text()<16) {
-        contenedor.append(img1+cont.text()+img2+archivo+img3+cont.text()+img4);
-        crearCuadros(cadena);
+        contenedor.append(img1+contadorGeneral+img2+archivo+img3+contadorGeneral+img4);
+        let contador = parseInt($("#cont").text())+1;
+        crearCuadros(cadena, contador);
         cont.text(+cont.text()+1);
     } else {
         console.log("Ocurrió un error :p");
@@ -31,16 +33,18 @@ $("body").on("change", "#inputFoto", function (event){
 function quitar (){
     let idTotal = $(event.target).attr("id");
     let id = idTotal.substring(6);
+    console.log(id);
     $("#"+id).remove();
     if (+cont.text() > 0){
         cont.text(+cont.text()-1);
         console.log(cont);
-        crearCuadros(cadena);
+        cadena[id] = null;
     }
+    let contador = parseInt($("#cont").text());
+    crearCuadros(cadena, contador);
 }
 
-function crearCuadros (cadena) {
-    let contador = parseInt($("#cont").text())+1;
+function crearCuadros (cadena, contador) {
     console.log(contador);
     let filas = Math.floor(Math.sqrt(contador*2));
     let columnas = 0;
@@ -66,12 +70,18 @@ function crearCuadros (cadena) {
         arreglo.push(m);
         arreglo.push(m);
     }
+    console.log(cadena);
+    let cadena2 = cadena.filter(elemento => elemento !== null);
+    console.log(cadena2);
     arreglo = shuffleArray(arreglo);
-    for (let k = 0; k < columnas;k++){
-        for (let j = 0; j < filas;j++){
-            console.log("Fila", j);
-            $("#tablero").append("<div class='col col-3'><div class='rectangulo p-3'><img style='width: 150px;' src='../imagenes/"+cadena[arreglo[k*filas+j]]+"'></div></div>");
+    if (filas !== 0){
+        for (let k = 0; k < columnas;k++){
+            for (let j = 0; j < filas;j++){
+                console.log("Fila", j);
+                $("#tablero").append("<div class='col col-3'><div class='rectangulo p-3'><img id='"+(k*filas+j)+"' onclick=\"destapar('"+cadena2[arreglo[k*filas+j]]+"',"+(k*filas+j)+")\" style='width: 150px;' src='../imagenes/"+cadena2[arreglo[k*filas+j]]+".png'></div></div>");
+            }
         }
+        $("#tablero").append("<div class='container'><button class='btn btn-primary'>Comenzar juego</button></div>");
     }
 }
 function shuffleArray(array) {
