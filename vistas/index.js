9d9d3def2216b524aeed28833b1bf436de1bc3ac
noe <noe.jaraq@gmail.com>
2023-06-28 19:40:28 -0500

    presistencia de imagenes seleccionadas

--- vistas/index.js
+++ vistas/index.js
@@ -9,13 +9,26 @@ let valor1 = null;
 let valor2 = null;
 let aciertos = 0;
 
-const img1 = "<div class='col-3 me-3' class='elemento' id='";
-const img2 = "'><div class='alert alert-dismissible fade show col-2' role='alert'>" +
-    "<img style='width: 200px;' src='../imagenes/";
-const img3= "' alt='imagen'>" +
-    "<button id='quitar";
+let localContador = localStorage.getItem("contador");
+let localImagenes = localStorage.getItem("imagenes") == null? []:JSON.parse(localStorage.getItem("imagenes"));
 
+const img1 = "<div class='col-3 me-3' class='elemento' id='";
+const img2 = "'><div class='alert alert-dismissible fade show col-2' role='alert'><img style='width: 200px;' src='../imagenes/";
+const img3= "' alt='imagen'><button id='quitar";
 const img4 = "' type='button' onclick='quitar()' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div></div>";
+
+if(localContador != null){ // ya existe una partida
+    cont.text(localContador);
+
+    // recargando las imagenes que ya habian
+    for(let i = 0; i < localImagenes.length; i++){
+        cadena.push(localImagenes[i].split('.').shift());
+        contenedor.append(img1+i+img2+localImagenes[i]+img3+i+img4)
+    }
+
+    crearCuadros(cadena, parseInt($("#cont").text()));
+
+}
 
 $("#botonAgregar").click(function () {
     $("#inputFoto").trigger('click');
@@ -26,11 +39,21 @@ $("body").on("change", "#inputFoto", function (event){
     let archivo = event.target.files[0].name;
     cadena.push(archivo.split('.').shift());
     let extension = archivo.split('.').pop();
+    
     if (extension === "png" && cont.text()<16) {
+
+        // guardando imagenes en storage
+        localImagenes.push(archivo);
+        localStorage.setItem("imagenes", JSON.stringify(localImagenes));
+
         contenedor.append(img1+contadorGeneral+img2+archivo+img3+contadorGeneral+img4);
         let contador = parseInt($("#cont").text())+1;
         crearCuadros(cadena, contador);
         cont.text(+cont.text()+1);
+
+        // guardando contador en storage
+        localStorage.setItem("contador", +localStorage.getItem("contador")+1);
+
     } else {
         console.log("Ocurrió un error :p");
     }
@@ -39,11 +62,20 @@ $("body").on("change", "#inputFoto", function (event){
 function quitar (){
     let idTotal = $(event.target).attr("id");
     let id = idTotal.substring(6);
-    console.log(id);
-    $("#"+id).remove();
+    //console.log(id);
+
+    // guardando nuevas imagenes y contador en storage
+    let caja = $("#"+id);
+    let archivo = caja.find("img").attr("src").split('/').pop();
+    let indice = localImagenes.indexOf(archivo);
+    localImagenes.splice(indice, 1);
+    localStorage.setItem("imagenes", JSON.stringify(localImagenes));
+    localStorage.setItem("contador", +localStorage.getItem("contador")-1);
+
+    caja.remove();
     if (+cont.text() > 0){
         cont.text(+cont.text()-1);
-        console.log(cont);
+        //console.log(cont);
         cadena[id] = null;
     }
     let contador = parseInt($("#cont").text());
@@ -51,7 +83,7 @@ function quitar (){
 }
 
 function crearCuadros (cadena, contador) {
-    console.log(contador);
+    //console.log(contador);
     let filas = Math.floor(Math.sqrt(contador*2));
     let columnas = 0;
     if(filas*(filas+1)===(contador*2)){
@@ -66,7 +98,7 @@ function crearCuadros (cadena, contador) {
     }else {
         filas = 0;
     }
-    console.log(filas, columnas);
+    //console.log(filas, columnas);
     $("#tablero").empty();
     $("#tablero").removeClass();
     $("#tablero").addClass("row row-cols-"+filas+" row-cols-lg-"+ columnas);
@@ -76,26 +108,28 @@ function crearCuadros (cadena, contador) {
         arreglo.push(m);
         arreglo.push(m);
     }
-    console.log(cadena);
+    //console.log(cadena);
     let cadena2 = cadena.filter(elemento => elemento !== null);
-    console.log(cadena2);
+    //console.log(cadena2);
     arreglo = shuffleArray(arreglo);
     if (filas !== 0){
         for (let k = 0; k < columnas;k++){
             for (let j = 0; j < filas;j++){
-                console.log("Fila", j);
+                //console.log("Fila", j);
                 $("#tablero").append("<div class='col col-3'><div class='rectangulo p-3' onclick=\"destapar('"+cadena2[arreglo[k*filas+j]]+"',"+(k*filas+j)+")\"><img style='height: 100px; display: none' id='img"+(k*filas+j)+"' src='../imagenes/"+cadena2[arreglo[k*filas+j]]+".png'></div></div>");
             }
         }
         $("#tablero").append("<div class='container' id='start'><button type='button' class='btn btn-primary'>Comenzar juego</button></div>");
     }
 }
+
 $(document).on('click', '#start', function (event) {
     console.log("en start");
     $("#contenedor").hide();
     $("#start").hide();
-    $("#tablero").append("<div class='container' id='buttons'><button type='button' id='random' class='btn btn-info'>Aleatorizar</button><button id='random' type='button' class='btn btn-warning'>Ayuda</button></div>");
+    $("#tablero").append("<div class='container' id='buttons'><button type='button' id='random' class='btn btn-info me-2'>Aleatorizar</button><button id='random' type='button' class='btn btn-warning'>Ayuda</button></div>");
 })
+
 function shuffleArray(array) {
     array.sort(() => Math.random() - 0.5);
     return array;
